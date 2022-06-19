fbe1d9b632e46e17e4d3241805296296ceb3905c
hanneshekl@gmail.com <hanneshekl@gmail.com>
2022-06-19 20:25:03 +0200

    Modified FabricExampleMod to have custom data.

--- src/main/java/net/treset/discman_cli/mixin/ExampleMixin.java
+++ src/main/java/net/treset/discman_cli/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.treset.discman_cli.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.treset.discman_cli.DiscmanClientMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		DiscmanClientMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
